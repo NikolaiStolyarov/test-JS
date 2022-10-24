e8b9a5f9fc1c9cd2230133167d196994105c498a
Nikolai <deadliner.nik@gmail.com>
2022-10-25 00:42:16 +0300

    Practice 1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
-"use sctrict";
-const category = 'toys';
-console.log(`https://someurl.com/${category}/5`);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оценить его?', ''),
+      c = prompt('Один из последних просмотренных фильмов', ''),
+      d = prompt('на сколько оценить его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
